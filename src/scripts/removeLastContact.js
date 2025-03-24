65df53483ee9a43641d2bdc4220e5a3833616bc5
AskAfter <askafter904@gmail.com>
2025-03-24 02:53:57 +0100

    homework done

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,10 @@
-export const removeLastContact = async () => {};
+import { writeContacts } from '../utils/writeContacts.js';
+import { readContacts } from '../utils/readContacts.js';
+
+export const removeLastContact = async () => {
+  const data = await readContacts();
+  data.pop();
+  await writeContacts(data);
+};
 
 removeLastContact();
